21f5221ebc96d8f423ee0b40b31ac70820081485
Vincent Palomo <vincentpalomo@yahoo.com>
2024-07-10 15:56:04 -0500

    added text to main cta

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>plenitudo solutions</div>
+      <div>
+        <div>Empowering seamless healthcare experiences with cutting-edge technology</div>
+        <div>plenitudo solutions</div>
+      </div>
     </main>
   );
 }
